d77ee68f5f766b3b8ffe9a4325d4710801931011
akk19 <111444622+akk19@users.noreply.github.com>
2022-08-21 08:35:27 +0530

    Add files via upload

---
+++ 00_productapp_why_di/src/main/java/com/demo/Main.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class Main {
+	public static void main(String[] args) {
+
+		@SuppressWarnings("resource")
+		ApplicationContext applicationContext=
+				new ClassPathXmlApplicationContext("demo.xml");
+		Passanger passanger=applicationContext.getBean("passanger", Passanger.class);
+		passanger.travel();
+		
+	}
+
+}
